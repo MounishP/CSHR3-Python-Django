2c5372549f504d8bce8032339d5d2c4c5d392fa8
shashank <shashank.dyavanapally@gmail.com>
2024-02-19 20:26:07 +1100

    Assignments

---
+++ factorial.py
@@ -0,0 +1,7 @@
+####Factorial####
+n = int (input ("Enter a number: "))
+factorial = 1
+if n >= 1:
+    for i in range (1, n+1):
+        factorial=factorial *i
+print("Factorial of the given number is: ", factorial)
