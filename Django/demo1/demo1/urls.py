5f23172ac8af2c34a013021c6d95420b3c17cbde
Sampath kumar <jettysampath8@gmail.com>
2024-04-07 14:23:43 +0530

    07/04/2024 Demo App creation

--- Django/demo1/demo1/urls.py
+++ Django/demo1/demo1/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from appdemo.views import homepage,hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', homepage, name='homepage'),
+    path('hello/', hello, name="hello")
 ]
